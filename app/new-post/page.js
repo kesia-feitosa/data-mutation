b97af46fb51383dbd18e5705430ececba530ec8d
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-10-28 09:44:22 -0400

    Add a server side function

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,44 +1,8 @@
-
 import PostForm from "@/components/post-form";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
+import { createPost } from "../actions/posts";
 
 export default function NewPostPage() {
   //client-side form action
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required.");
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required.");
-    }
-
-    if (!image || image.size === 0) {
-      errors.push("Image is required.");
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    //await is added to wait finish the insert function ends to redirect
-    await storePost ({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-
-    redirect('/feed');
-  }
-
+  
   return <PostForm action={createPost} />;
 }
