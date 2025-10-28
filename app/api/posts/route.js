18e89c646648a0caba2566ba1261d3bf397902d1
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-10-27 21:00:13 -0400

    Save data and server side validations

---
+++ app/api/posts/route.js
@@ -0,0 +1,4 @@
+//POST api, can be used by outside programs
+export function POST(request) {
+
+}
--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,30 +1,44 @@
+
+import PostForm from "@/components/post-form";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
 export default function NewPostPage() {
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
+  //client-side form action
+  async function createPost(prevState, formData) {
+    "use server";
+    const title = formData.get('title');
+    const image = formData.get('image');
+    const content = formData.get('content');
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required.");
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required.");
+    }
+
+    if (!image || image.size === 0) {
+      errors.push("Image is required.");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    //await is added to wait finish the insert function ends to redirect
+    await storePost ({
+      imageUrl: '',
+      title,
+      content,
+      userId: 1
+    })
+
+    redirect('/feed');
+  }
+
+  return <PostForm action={createPost} />;
 }
